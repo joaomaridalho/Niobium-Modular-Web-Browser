64efa5a1781022432844db2282249be3f8249220
unknown <Joao31246@eslcsec.lan>
2016-02-25 10:16:20 +0000

    Bugs Fixed

--- public/js/browser.js
+++ public/js/browser.js
@@ -6,18 +6,22 @@ $('#navbar').keyup(function (e) {
             url = "http://" + url;
         }
         $('.vista.ativa').attr('src', url);
-        $('.tab.ativa').html(url + "<a>X</a>");
+        $('.tab.ativa').html("<a>X</a>");
     }
 });
+
 $(".addsep").click(function () {
     $(".tab,.ativa").removeClass("ativa");
     $(".vista,.ativa").removeClass("ativa");
-    $("#separadores").append("<li class='tab ativa'>Youtube<a>X</a></li>");
-    $("#views").append("<webview class='vista ativa' src=" + "http://youtube.com" + "></webview>");
-
+    $("#separadores").append("<li class='tab ativa'><a>X</a></li>");
+    $("#views").append("<webview class='vista ativa' src=" + "http://google.com" + "></webview>");
+    $("#at").removeAttr("id");
+    $(".vista.ativa").attr("id", "at");
     var n = $("#separadores .tab").length;
     var w = (100 / n) - 3;
     $(".tab").width(w + '%');
+    $("#at.vista.ativa").bind("dom-ready", title);
+    $("#at.vista.ativa").bind("new-window", novo);
 });
 $("body").on("click", ".tab", function () {
     $(".tab,.ativa").removeClass("ativa");
@@ -36,3 +40,17 @@ $("body").on("click", ".tab a", function () {
     var w = (100 / n) - 3;
     $(".tab").width(w + '%');
 });
+var title = function () {
+    console.log("ola");
+    var pag = document.getElementById("at").getTitle();
+    var url = document.getElementById("at").getURL();
+    var o = $("#at").index();
+    $('.tab').eq(o).html("<img class='timg' src = 'https://plus.google.com/_/favicon?domain_url=" + url + "'>" + "<div class=out>" + pag + "</div>" + "<a>X</a>");
+};
+var novo = function (e) {
+    $(".addsep").trigger("click");
+    $('.vista.ativa').attr('src', e.url);
+}
+
+$("#at.vista.ativa").bind("dom-ready", title);
+document.getElementById("at").addEventListener("new-window", novo);
